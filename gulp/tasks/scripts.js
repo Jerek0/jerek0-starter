7595f76535176155ddc3b423a42f801ff1ddc298
Jerek0 <jeremy.minie@gmail.com>
2016-09-21 20:09:21 +0100

    Bugfix ES6 source maps + handleErrors on `cleanJs`

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -13,16 +13,18 @@ import handleErrors from'../util/handleErrors';
 
 gulp.task('cleanJs', () => {
     gulp.src(config.scripts.dest, {read: false})
-        .pipe(clean());
+        .pipe(clean())
+        .on('error', handleErrors);
 });
 
 gulp.task('scripts:dev', ['cleanJs'], () => {
-  return browserify({ entries: config.scripts.main, debug: false })
-    .transform(babelify.configure())
+  return browserify({ entries: config.scripts.main, debug: true })
+    .transform(babelify.configure({presets: ['es2015'], sourceMaps: true}))
     .bundle().on('error', handleErrors)
     .pipe(source('min.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(config.scripts.dest))
     .pipe(browserSync.reload({stream:true}));
@@ -31,7 +33,7 @@ gulp.task('scripts:dev', ['cleanJs'], () => {
 
 gulp.task('scripts:prod', ['cleanJs'], () => {
   return browserify({ entries: config.scripts.main, debug: false })
-    .transform(babelify.configure())
+    .transform(babelify.configure({presets: ['es2015']}))
     .bundle().on('error', handleErrors)
     .pipe(source('min.js'))
     .pipe(buffer())
